3e13c28c5a69ef4894195d8b50edc94c266cd533
pbuterba <pbuterba.corp@gmail.com>
2024-08-03 17:40:16 -0400

    Add custom exceptions

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 from htmlwriter.document import Document, Doctype
 from htmlwriter.node import Node
+from htmlwriter.exceptions import HTMLWriterException, NodeTypeException, DOMTreeException
 
 Document = Document
 Doctype = Doctype
+
 Node = Node
+
+HTMLWriterException = HTMLWriterException
+NodeTypeException = NodeTypeException
+DOMTreeException = DOMTreeException
---
+++ exceptions.py
@@ -0,0 +1,30 @@
+"""
+@package    htmlwriter
+@brief      Contains custom exceptions to be used in the package
+
+@date       8/3/2024
+@updated    8/3/2024
+
+@author     Preston Buterbaugh
+"""
+
+
+class HTMLWriterException(Exception):
+    """
+    @brief  General exception to be used by the HTML writer
+    """
+    pass
+
+
+class NodeTypeException(Exception):
+    """
+    @brief  Exception for a mismatch between expected and actual node types (text, tree, or contentless)
+    """
+    pass
+
+
+class DOMTreeException(Exception):
+    """
+    @brief  An exception for unexpected conditions in a DOM tree
+    """
+    pass
